1eed91a5faa6fe3103df3eb43fa5aad85b6851f4
Paul <espinozapauln@gmail.com>
2020-12-25 20:08:10 -0600

    Migrated to Postgress

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'leadmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'leadmanagerdb',
+        'USER': 'postgres',
+        'PASSWORD': 'Django1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -127,3 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
